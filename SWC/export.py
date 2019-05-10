0fcd3da7841c1cee4b0bc3a398082ef732983322
Padraig Gleeson <p.gleeson@gmail.com>
2019-05-10 13:57:30 +0100

    Adding more cells

--- SWC/export.py
+++ SWC/export.py
@@ -1,6 +1,11 @@
 from pyneuroml.swc.ExportSWC import convert_to_swc
 
-files = ['../nC_projects_lite/PN1LS_allORNs/generatedNeuroML2/neuron_PN1_LS_sk_419138.cell.nml']
+files = ['../nC_projects_lite/PN1LS_allORNs_Rinput/generatedNeuroML2/neuron_PN1_LS_sk_419138.cell.nml',
+         '../nC_projects_lite/PN1LS_allORNs/generatedNeuroML2/neuron_PN1_LS_sk_419138.cell.nml',
+         '../nC_projects_lite/PN1RS_allORNs/generatedNeuroML2/neuron_PN1_RS_sk_638603.cell.nml',
+         '../nC_projects_lite/PN2LS_allORNs/generatedNeuroML2/neuron_PN2_LS_sk_427345.cell.nml',
+         '../nC_projects_lite/PN2RS_allORNs/generatedNeuroML2/neuron_PN2_RS_sk_480245.cell.nml',
+         '../nC_projects_lite/PN3LS_allORNs/generatedNeuroML2/neuron_PN3_LS_sk_668267.cell.nml']
 
 for f in files:
     import os 
